4dbd763eb83eafd2e71fe9309a630c47a716c9fa
anonymous anonymous <juliejychung@gmail.com>
2021-12-03 00:08:21 +0000

    Direct association between courts and matches

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Match, type: :model do
   
     describe "Direct Associations" do
 
+    it { should belong_to(:court) }
+
     it { should have_many(:comments) }
 
     it { should belong_to(:opponent) }
